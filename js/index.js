034cc222d2c95df85f7a3f168471d6005de9c3a0
aaldossary <35977760+aaldossary@users.noreply.github.com>
2018-03-07 15:24:29 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,57 @@
+//my file
+
+console.log("..the file before loaded");
+
+//document.onload = doEverting();
+
+$(document).ready(function() {
+
+
+// ceate a loop to print out all elmenets in the DOm 
+//.. 1. ctrteloop strucature. how to fihure out the length, i.e., the number of itrtations of my loop
+
+//2. how do i access a element from my page .. then HTML elemnts?
+//3. how do I want to prinnt them 
+
+
+
+	$("#close.btn").click(function(){
+	$('#not').hide(100);
+
+});
+
+
+
+	$("button").click(function(){
+	$('p').toggle();
+
+
+});
+
+// use jS to ide and soow
+ 
+
+console.log("loaded & done");
+
+$(".butoms").click(function(){
+		$(#'not').hide(1000);
+
+});
+
+
+function abdo (){
+document.querySelector('h1').innerHTML = "Everyhing done!!";
+
+	$('h1').text ("now JQuery").addClass('red');
+
+
+	var el = document;
+	console.log(el);
+}
+
+abdo();
+
+
+
+
+});
